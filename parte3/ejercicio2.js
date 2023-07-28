21369c56563d4fa25e27425ac93d73cace5a6e26
Martin Cardozo <tiin.-@hotmail.es>
2023-07-28 00:16:21 -0300

    se agrega el ejercicio numero 2 y se completa el tpn3

--- parte3/ejercicio2.js
+++ parte3/ejercicio2.js
@@ -1,18 +1,41 @@
-
 /*
-Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+Ejemplo:
+Input:
+[‘Nueva York, Estados Unidos’, ‘Barcelona, España’, ‘Tokio, Japón’, ‘Londres, Reino Unido’, ‘Roma, Italia’, ‘Pekín, China’, ‘Río de Janeiro, Brasil’, ‘Ámsterdam, Países Bajos’, ‘Sídney, Australia’, ‘El Cairo, Egipto’]
+
 */
 
 
 
-let num = parseInt(prompt("Ingrese un numero"));
- 
-const esParImpar = (num) =>{
-    if(num % 2 === 0){
-        return alert("el numero es par")
-      }else{
-        return alert("el numero es impar")
-      }
-}
 
-esParImpar(num);
+let input;
+const ciudades = [];
+let  paris = "paris";
+while (true) {
+  input = prompt("ingrese dato:");
+  if(input === null){
+    break
+  }
+  ciudades.push(input);
+}
+ciudades.splice(ciudades.length -1);
+console.log("Array original")
+console.log(ciudades);
+console.log("Longitud del Array")
+console.log("El array tiene una longitud de : " + ciudades.length);
+console.log("la posicion 1, 2 y la ultima ") 
+console.log(ciudades[0],ciudades[1],ciudades[ciudades.length - 1],);
+console.log("se agrega a paris en la ultima posicion")
+console.log(ciudades.push(paris));
+console.log(ciudades);
+console.log("Elemento en la segunda posicion es " + ciudades[1]);
+ciudades[1] = "barcelona";
+console.log("la segunda posicion fue modificada " + ciudades);
