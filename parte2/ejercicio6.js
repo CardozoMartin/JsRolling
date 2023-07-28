67a692e47a09ee2d8f7fbd7480673f0fc6a958ff
Martin Cardozo <tiin.-@hotmail.es>
2023-07-27 23:38:24 -0300

    se modifica un ejercicio

--- parte2/ejercicio6.js
+++ parte2/ejercicio6.js
@@ -2,12 +2,12 @@
 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30)
 */
 
-for (let i = 30; i > 0; i--) {
-    let piramide = "";
+for (let i = 1; i <= 10; i++) {
+    let linea = "";
   
     for (let j = 1; j <= i; j++) {
-        piramide += i ;
+      linea += i ;
     }
   
-    console.log(piramide);
-     }
+    console.log(linea);
+  }
