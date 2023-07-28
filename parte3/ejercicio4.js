995337a1d52ffe03e219afdafc9a9dc1391dc89c
Martin Cardozo <tiin.-@hotmail.es>
2023-07-28 00:08:34 -0300

    se carga el ejercicio N4

---
+++ parte3/ejercicio4.js
@@ -0,0 +1,17 @@
+/*
+Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+*/
+
+
+
+let num = parseInt(prompt("Ingrese un numero"));
+ 
+const esParImpar = (num) =>{
+    if(num % 2 === 0){
+        return alert("el numero es par")
+      }else{
+        return alert("el numero es impar")
+      }
+}
+
+esParImpar(num);
